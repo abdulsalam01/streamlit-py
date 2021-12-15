5bc5e4cd53906a65894802a0eda4296bfae78da2
jok3r46 <jsuf046@gmail.com>
2021-12-15 14:45:01 +0700

    data-fix

--- main.py
+++ main.py
@@ -2,50 +2,36 @@ import plotly.express as px
 import streamlit as st
 import pandas as pd
 
-df = pd.read_csv('produksi_minyak_mentah.csv')
+df_csv = pd.read_csv('produksi_minyak_mentah.csv')
 df_json = pd.read_json('kode_negara_lengkap.json')
-df_merger = {}
+df_json.to_csv("json.csv", index=False)
+csv_json = pd.read_csv("json.csv")
 
-for json in df_json.iterrows():
-    alpha = json[1]['alpha-3']
-    name = json[1]['name']
-    region = json[1]['region']
-    subregion = json[1]['sub-region']
- 
-    for row in df.iterrows():
-        if row[1]['kode_negara'] == alpha:
-            df_merger['name'] = name
-            df_merger['alpha-3'] = alpha
-            df_merger['region'] = region
-            df_merger['sub-region'] = subregion
-            df_merger['kode_negara'] = row[1]['kode_negara']
-            df_merger['tahun'] = row[1]['tahun']
-            df_merger['produksi'] = row[1]['produksi']
-   
+
 # print-all
-#print(df_merger)
+df = df_csv.merge(csv_json, left_on='kode_negara', right_on='alpha-3')
 
-# All Brands
+# select-filter
 st.sidebar.header("Pilih Filter:")
-kode_negara = st.sidebar.multiselect(
+name_negara = st.sidebar.multiselect(
     "Pilih Negara:",
-    options=df_merger["kode_negara"].unique(),
-    default=df_merger["kode_negara"].unique(),   
+    options=df['name_negara'].unique(),
+    default=df['name_negara'].unique(),
 )
 
 tahun = st.sidebar.multiselect(
     "Pilih Tahun:",
-    options=df_merger["tahun"].unique(),
-    default=df_merger["tahun"].unique(),   
+    options=df["tahun"].unique(),
+    default=df["tahun"].unique(),
 )
 
-#Filter
-filter_data = df_merger.query(
-    "kode_negara == @kode_negara & tahun == @tahun"
+# Filter
+filter_data = df.query(
+    "name_negara == @name_negara & tahun == @tahun"
 )
 
-#Chart
-data_pertahun = filter_data.groupby(by='kode_negara').sum()[['produksi']]
+# Chart
+data_pertahun = filter_data.groupby(by='name_negara').sum()[['produksi']]
 
 fig_data = px.bar(
     data_pertahun,
